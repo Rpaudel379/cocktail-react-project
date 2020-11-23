b6a2941ba991efcea6340d275bef8335d6a19c29
anish <rpaudel379@gmail.com>
2020-11-23 23:44:13 +0530

    inital commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+        accusamus consequuntur obcaecati unde repellendus, voluptates cupiditate
+        officiis. Culpa necessitatibus neque libero ratione labore nobis iusto
+        facilis, suscipit id vel quasi incidunt cupiditate fuga amet optio.
+        Distinctio amet facilis sequi perferendis?
+      </p>
+    </section>
+  );
+};
+
+export default About;
